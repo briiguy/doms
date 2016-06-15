f6e447f0537be0e5e50782140a44be9964020061
briiguy <brianevasiuk@gmail.com>
2016-06-15 15:53:48 -0500

    Add files via upload

--- dom-manipulations/main.js
+++ dom-manipulations/main.js
@@ -4,58 +4,99 @@ document.querySelector("#thanks button").addEventListener('click',function(){
   alert("yowch! don't click me so hard!")
 })
 
+
+//TASK #2
 document.querySelector("#double button").addEventListener('click',function(){
   var dub=document.querySelector("#compoundInvestment")
 	dub.textContent=dub.textContent*2
 
 })
 
-
+//  TASK #3
 document.querySelector("#color-circle button").addEventListener('click',function(){
+
+
 	var circle=document.querySelector("#circle-bw")
 	var computedStyle = window.getComputedStyle(circle)
-	if(computedStyle.background='rgb(255,255,255)'){computedStyle.background=(computedStyle.background='black')}
-		else{computedStyle.background=computedStyle.background='rgb(255,255,255)'}
-  // TASK #3
+
+	if(circle.style.background==='white'){circle.style.background='black'}
+	else {circle.style.background='white'}
+
 })
 
+
+  // TASK #4
 document.querySelector("#blow-up button").addEventListener('click',function(){
-  // TASK #4}
-  var blowUp=document.querySelector("#blow-up")
-  var computedStyle = window.getComputedStyle(blowUp)
-  if(computedStyle.answerBox.circleRed.width!='230px'){
-  	computedStyle.answerBox.circleRed.width=parseInt(computedStyle.answerBox.circleRed.width)*2+'px'
-	computedStyle.answerBox.circleRed.height=parseInt(computedStyle.answerBox.circleRed.height)*2+'px'
-  }
-  else{computedStyle.answerBox.circleRed.width='40px' computedStyle.answerBox.circleRed.height='40px'}
+	console.log('working function')
+	// select the red circle 
+	var blowUp=document.querySelector("#blow-up .circle-red")
+	var computedStyle = window.getComputedStyle(blowUp)
+
+	if (computedStyle.width != '320px') {
+		blowUp.style.width = parseInt(computedStyle.width) * 2 + 'px'
+		blowUp.style.height = parseInt(computedStyle.height) * 2 + 'px'
+	}
+	else { 
+		blowUp.style.width = '40px' 
+		blowUp.style.height = '40px'
+	}
 })
 
 document.querySelector("#remove button").addEventListener('click',function(){
-	var userList=document.querySelector("#userList")
-	var liNodes=document.querySelector("#userList>li")
-	document.querySelector("#color-circle button")
-	var remove = function() {
-  for (var i = 0; i < liNodes.length; i ++) {
-    var liNode = liNodes[i]
-    if (liNode.classList.contains('inactive')) {
-       container.removeChild(liNode)
-    } 
-  }
+	var userList=document.querySelectorAll("#userList li")
+	// var liNodes=document.querySelector("#userList li")
+	
+for(var i=0;i<userList.length;i++){
+	
+	if(userList[i].textContent==='inactive user'){
+		
+		userList[i].parentNode.removeChild(userList[i])
+
+	}
+
 }
-  // TASK #5
+
+
+
 })
 
 
 document.querySelector("#reverse-squares button").addEventListener('click',function(){
-	var reverseSquares=document.querySelector("#reverseSquares .answerBox")
-	reverseSquares.innerHTML=
-  // TASK #6
-})
+	var spanNodes=document.querySelectorAll("span")
+	console.log(spanNodes)
+	var containerNode=document.querySelector("#solution")
+	for(var i=spanNodes.length-1;i>=0;i--){
 
-document.querySelector("#pig-latin button").addEventListener('click',function(){
-  // TASK #7
-})
+		var oneSpan=spanNodes[i]
+		oneSpan.parentNode.removeChild(oneSpan)
+		console.log(containerNode)
+		containerNode.appendChild(spanNodes[i])
+	}
+//   // TASK #6
+ })
 
-document.querySelector("#cycle-image button").addEventListener('click',function(){
-  // TASK #8
-})
+// document.querySelector("#pig-latin button").addEventListener('click',function(){
+//   // TASK #7
+// })
+
+// document.querySelector("#cycle-image button").addEventListener('click',function(){
+//   // TASK #8
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
